e1dac779f2dbc5dd9869c3b3bcd93aed8e23fc0e
jalper1 <alperjordan@gmail.com>
2024-04-29 14:21:24 -0500

    updated website styling

--- holos1website/src/Forum/ForumMessage.jsx
+++ holos1website/src/Forum/ForumMessage.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import "./forumMessage.css";
 
 function ForumMessage(props) {
-
-
-    return (
-        <Card style={{ margin: "0.5rem", padding: "0.5rem" }}>
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>{props.content}</Card.Text>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className="card-forum">
+      <Card.Body className="card-body-forum">
+        <Card.Title className="card-title">{props.title}</Card.Title>
+        <Card.Text className="card-text">{props.content}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default ForumMessage;
