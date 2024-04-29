3c7990a0e080baeea61e4aed7adf84ca7b00c42d
yyllzora <158620065+yyllzora@users.noreply.github.com>
2024-04-28 23:05:23 -0500

    Update AboutUs.jsx

--- holos1website/src/About Us Page/AboutUs.jsx
+++ holos1website/src/About Us Page/AboutUs.jsx
@@ -1,19 +1,23 @@
 import NavBar from "../NavBar/NavBar";
 import "./aboutUs.css";
 import Card from "../Card/Card";
-import image from "../assets/holos.jpg";
+import NishanthImage from "../assets/Nishanth.jpg";
+import SakshamImage from "../assets/Saksham.jpg";
+import WeibingImage from "../assets/Weibing.jpg";
+import YuyangImage from "../assets/Yuyang.JPG";
+import JordanImage from "../assets/Jordan.jpg"; // This line should correctly import the image
 
 function AboutUs() {
   return (
     <>
       <NavBar />
-      <header className="about-header">About Us Page</header>
-      <div className="cards">
-        <Card name="Jordan" description="Description Here" image={image} />
-        <Card name="Nishanth" description="Description Here" image={image} />
-        <Card name="Saksham" description="Description Here" image={image} />
-        <Card name="Weibing" description="Description Here" image={image} />
-        <Card name="Yuyang" description="Description Here" image={image} />
+      <header className="about-header">We're more than just a student team. We've got goals, hopes, and dreams, just like you...</header>
+      <div className="team-container">
+        <Card name="Nishanth Naik" description="Description Here" image={NishanthImage} />
+        <Card name="Saksham Jain" description="Description Here" image={SakshamImage} />
+        <Card name="Weibing Wang" description="Description Here" image={WeibingImage} />
+        <Card name="Yuyang Liu" description="I'm a senior at the University of Wisconsin-Madison, and through VR, I explore new possibilities, and it's a good idea to make learning and entertainment come alive!" image={YuyangImage} />
+        <Card name="Jordan Alper" description="Hi! My name’s Jordan Alper and I’m a senior at UW-Madison! I enjoy developing video games, so a VR project was a really cool experience for me, as it uses Unity as well!" image={JordanImage} />
       </div>
     </>
   );
